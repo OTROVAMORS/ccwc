ab193683b0c2aa0222d755b7df9e021152a932a2
Aron Ribeiro Chaves <aron.ribeiro.chaves@gmail.com>
2023-11-28 19:57:49 -0300

    feature: CountAndPrint
    get the filePath and executes one of the parameter functions

--- lib/utils.go
+++ lib/utils.go
@@ -7,3 +7,14 @@ import (
 	"unicode"
 )
 
+func CountAndPrint(filePath string, countBytes, countLines, countWords, countCharacters bool) {
+    data, err := ReadFile(filePath)
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+
+    fmt.Println("File Analysis:")
+    ProcessData(data, countBytes, countLines, countWords, countCharacters)
+}
+
