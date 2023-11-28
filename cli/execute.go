3f73d1b2f53dcda0436bf6e01486da5d3682dc24
Aron Ribeiro Chaves <aron.ribeiro.chaves@gmail.com>
2023-11-28 19:55:34 -0300

    setting CLI flags

--- cli/execute.go
+++ cli/execute.go
@@ -11,3 +11,10 @@ import (
 
 
 func Execute() {
+
+	countBytes := flag.Bool("c", false, "Count bytes")
+	countLines := flag.Bool("l", false, "Count lines")
+	countWords := flag.Bool("w", false, "Count words")
+	countCharacters := flag.Bool("m", false, "Count characters")
+	flag.Parse()
+
